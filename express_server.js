f421468ab0c1f7cad7f0fe8cf989b928cc698086
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-16 02:21:59 +0000

    its alive! fully implemented url shortening and redirection :D

--- express_server.js
+++ express_server.js
@@ -54,17 +54,23 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 app.post("/urls", (req,res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const shortURL = randomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 // points to specific short url
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params.shortURL)
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL)
+});
 
 
 // lets server listen, end of server functionality 

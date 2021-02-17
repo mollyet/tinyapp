57356e07b3876e847fec9322d5f716ff8bcaebe3
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-17 22:01:43 +0000

    created registration from (to be tweaked for #aesthetics), and added get request

--- express_server.js
+++ express_server.js
@@ -115,6 +115,15 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls")
 })
 
+//registration
+
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"]
+  }
+  res.render("register", templateVars)
+})
+
 //error pages
 
 app.get("*", (req,res) => {
